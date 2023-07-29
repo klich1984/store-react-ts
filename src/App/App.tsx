a772854ee171b3014f0ae2b42023f1f0538c4c9a
klich84 <klich84@gmail.com>
2023-07-29 11:36:59 -0500

    :gear: Install react router Dom and create routes.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,26 +1,32 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
 import Home from '../Pages/Home'
 import MyAccount from '../Pages/MyAccount'
 import MyOrder from '../Pages/MyOrder'
 import MyOrders from '../Pages/MyOrders'
 import NotFound from '../Pages/NotFound'
+import SingIn from '../Pages/SingIn'
+
 import './App.css'
 
-function App() {
-  return (
-    <>
-      <h1 className='text-rose-500 '>Hi, klich 84</h1>
-      <hr />
-      <Home />
-      <hr />
-      <MyAccount />
-      <hr />
-      <MyOrders />
-      <hr />
-      <MyOrder />
-      <hr />
-      <NotFound />
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: '/', element: <Home />},
+    { path: '/my-account', element: <MyAccount />},
+    { path: '/my-order', element: <MyOrder />},
+    { path: '/my-orders', element: <MyOrders />},
+    { path: '/sign-in', element: <SingIn />},
+    { path: '/*', element: <NotFound />},
+  ])
 
-    </>
+  return routes
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
 
